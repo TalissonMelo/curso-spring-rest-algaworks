bd11f253f255c1ed70fad5261018de85bd0e2b5c
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-04-22 10:05:02 -0300

    Inserindo Cliente com POST

--- src/main/java/com/talissonmelo/osworks/api/controller/CustomersController.java
+++ src/main/java/com/talissonmelo/osworks/api/controller/CustomersController.java
@@ -1,13 +1,17 @@
 package com.talissonmelo.osworks.api.controller;
 
 import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.talissonmelo.osworks.domain.model.Customers;
@@ -17,9 +21,6 @@ import com.talissonmelo.osworks.domain.repository.CustomersRepository;
 @RequestMapping(value = "/customers")
 public class CustomersController {
 	
-	@PersistenceContext
-	private EntityManager manager;
-	
 	@Autowired
 	private CustomersRepository customersRepository;
 
@@ -27,6 +28,23 @@ public class CustomersController {
 	public List<Customers> findAll() {
 		return customersRepository.findAll();
 		//return customersRepository.findByNameContaining("Ta");
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Customers> findById(@PathVariable Long id) {
+		Optional<Customers> client = customersRepository.findById(id);
+		
+		if(client.isPresent()) {
+			return ResponseEntity.ok().body(client.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Customers insert(@RequestBody Customers client) {
+		return customersRepository.save(client);
 	}
 
 }
